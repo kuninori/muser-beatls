5a216dc64b1b8e1cf5fe7852002d3069859f459c
藤井邦憲 <kuninori@lggr.jp>
2020-07-15 20:02:06 +0900

    add auth0 button

--- src/index.js
+++ src/index.js
@@ -3,19 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Amplify,{API} from "aws-amplify";
+import Amplify, {API} from "aws-amplify";
 import awsExports from "./aws-exports";
+import {useAuth0, Auth0Provider} from "@auth0/auth0-react";
+
 Amplify.configure(awsExports);
 
 API.get("apib7805dd0")
-  .then((res)=>{
+  .then((res) => {
     console.log(res)
   });
 
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={"logger-test.us.auth0.com"}
+      clientId={"4i5eVcaT9bFSuhgYGewlcHbyy7u9hV74"}
+      redirectUri={window.location.origin}
+    >
+      <App/>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
